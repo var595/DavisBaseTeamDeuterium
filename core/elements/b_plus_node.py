780a8faee69b44a718c1ec71edb29b598816127e
Jaival Upadhyay <57015292+jaivalupadhyay@users.noreply.github.com>
2023-12-09 03:40:27 -0600

    Updated Code

--- core/elements/b_plus_node.py
+++ core/elements/b_plus_node.py
@@ -3,6 +3,13 @@ from core.elements.b_tree_node import b_tree_node
 
 class b_plus_node(b_tree_node):
     def __init__(self, leaf: bool, parent: b_tree_node) -> None:
+        """
+        Initialize a B+ tree node.
+
+        Args:
+        - leaf: Indicates if the node is a leaf node or not.
+        - parent: Parent node reference.
+        """
         super().__init__(leaf)
         self.pointers: List[b_plus_node] = []
         self.parent: b_plus_node = parent
